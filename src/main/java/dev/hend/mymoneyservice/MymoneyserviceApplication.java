0b4c0ed67b301b029b1a7a7f00aac99da8214dff
Patrick Henderson <pathend98@gmail.com>
2024-01-11 07:49:57 +0000

    chore: create project from spring initializr

---
+++ src/main/java/dev/hend/mymoneyservice/MymoneyserviceApplication.java
@@ -0,0 +1,13 @@
+package dev.hend.mymoneyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymoneyserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymoneyserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/hend/mymoneyservice/MymoneyserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hend.mymoneyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymoneyserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
